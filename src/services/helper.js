8ac8ab9a28db183948cbdeecb8ffe223519be168
Shashwat Pati <shashwatpati02@gmail.com>
2024-01-08 00:57:17 +0530

    new commit

--- src/services/helper.js
+++ src/services/helper.js
@@ -1,4 +1,3 @@
 // export const BASE_URL = "http://localhost:5000";
 // export const BASE_URL = "https://snack-box-back.onrender.com";
-export const BASE_URL =
-    "food-delivery-3uh8zhnmm-shashwat-patis-projects.vercel.app";
+export const BASE_URL = "https://food-delivery-app-five-nu.vercel.app/";
